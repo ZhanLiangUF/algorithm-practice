43ed0d66496ce51f7fb26bb1e0f924c22cc57f92
unknown <zhanliang1994@gmail.com>
2019-04-09 13:30:12 -0400

    Finish original solution for elimination game

--- src/EliminationGame.java
+++ src/EliminationGame.java
@@ -9,7 +9,25 @@ Find the last number that remains starting with a list of length n.
 */
 
 public class EliminationGame {
+  // first solution time limit exceeds
   public int lastRemaining(int n) {
+          List<Integer> list = new ArrayList<Integer>();
+          for (int i = 1;i<=n;i++) {
+              list.add(i);
+          }
+          while (list.size() != 1) {
+              n = n % 2 == 0 ? n/2:(n/2)+1;
+              for (int i = 0;i<n;i++) {
+                  list.remove(i);
+              }
+              // reverse the list instead of changing indexes for second pattern
+              Collections.reverse(list);
+              n = list.size();
+          }
+          return list.get(0);
+  }
+  
+  public int lastRemaininglogN(int n) {
 
   }
 }
