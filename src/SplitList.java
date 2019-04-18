cdc00f9bfbfc67c4622a0c07c92016cb55d63d7f
unknown <zhanliang1994@gmail.com>
2019-04-18 12:06:24 -0400

    finish splitting list

--- src/SplitList.java
+++ src/SplitList.java
@@ -7,8 +7,13 @@ public class SplitList {
             len++;
         int n = len / k, r = len % k; // n : minimum guaranteed part size; r : extra nodes spread to the first r parts;
         ListNode node = root, prev = null;
-         for (int i = 0; node != null && i < k; i++) {
+         for (int i = 0; node != null && i < k; i++, r--) {
             parts[i] = node;
+            for (int j = 0; j < n + (r > 0 ? 1:0);j++) {
+              prev = node;
+              node = node.next;
+            }
+            prev.next = null;
         }
         return parts;
     }
