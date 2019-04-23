c6cd76af05981422dec2cd3da537d5f86d81abec
unknown <zhanliang1994@gmail.com>
2019-04-23 10:57:21 -0400

    finish recursive solution to sum root to leaf numbers

--- src/SumRootToLeafNumbers.java
+++ src/SumRootToLeafNumbers.java
@@ -1,12 +1,14 @@
 public class SumRootToLeafNumbers {
-      public int sumNumbers(TreeNode root) {
-          return sumNumbersHelper(root)
-      }
-      public static int sumNumbersHelper(TreeNode root, int s) {
-          if (root == null) {
-              return s
-          } else {
-              return sumNumbers(root.left, s +) + sumNumbers(root.right, s)
-          }
-      }
+  public int sumNumbers(TreeNode root) {
+    return sumNumbersHelper(root, 0);
+  }
+  public static int sumNumbersHelper(TreeNode root, int s) {
+    if (root == null) {
+      return 0;
+    } else if (root.left == null && root.right == null) {
+      return s*10 + root.val;
+    } else {
+      return sumNumbersHelper(root.right, s*10 + root.val) + sumNumbersHelper(root.left, s*10 + root.val);
+    }
+  }
 }
