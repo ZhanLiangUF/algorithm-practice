10c43efb42ccf2b1779a7e5669852eaacbbb4243
unknown <zhanliang1994@gmail.com>
2019-05-08 20:47:11 -0400

    add memoization for lcs

--- src/Lcs.java
+++ src/Lcs.java
@@ -11,4 +11,22 @@ public class LCS {
       return Math.max(lcs(X, Y,m,n-1), lcs(X, Y,m-1,n);
     }
   }
+  // memoization
+  public static int lcs(char[] X, char[] Y, int m, int n) {
+    int[][] arr = new int[n+1][m+1];
+    for (i = 0;i < m+1;i++) {
+      for (j = 0;j < n+1;j++) {
+        if (i == 0 || j == 0) {
+          arr[i][j] = 0;
+        }
+        if (X[i] == Y[j]) {
+          arr[i][j] = arr[i-1][j-1] + 1;
+        } else {
+          arr[i][j] = Math.max(arr[i-1][j], arr[i][j-1]);
+        }
+      }
+
+
+    }
+  }
 }
