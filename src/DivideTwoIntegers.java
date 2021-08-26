7f8947db8320af235754993e5229827289050cbb
Zhan Liang <zhanliang1994@gmail.com>
2021-08-26 14:55:46 -0400

    Implemented Divide Two Integers without using multiplication, modulo, and division

---
+++ src/DivideTwoIntegers.java
@@ -0,0 +1,20 @@
+class Solution {
+  public int divide(int dividend, int divisor) {
+      if (dividend == Integer.MIN_VALUE && divisor == -1) { return Integer.MAX_VALUE; }
+      
+      // USE XOR to determine if negative 
+      boolean negative = dividend < 0 ^ divisor < 0;
+      
+      dividend = Math.abs(dividend);
+      divisor = Math.abs(divisor);
+      
+      int quotient = 0, subQuot = 0;
+      
+      while (dividend - divisor >= 0) {
+          for (subQuot = 0; dividend - (divisor << subQuot << 1) >= 0; subQuot++);
+          quotient += 1 << subQuot;
+          dividend -= divisor << subQuot;
+      }
+      return negative ? -quotient : quotient;
+  }
+}
